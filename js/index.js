9a858fcdbdd77082f891f05f189d919a52eb17a7
unknown <juansebastian.012@gmail.com>
2018-12-01 11:32:37 -0300

    SteamPublisherGenerator;

--- js/index.js
+++ js/index.js
@@ -19,26 +19,26 @@ ISteamUser: Steam provides API calls to provide information about Steam users.
 ITFItems_440: Team Fortress 2 provides API calls to use when accessing player item data.
 */
 
-
+// install https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi?hl=es
+// for Allow-Control-Allow-Origin: *
 
 var publishers;
 var appitemid;
 var appname;
 
-$('#start').on('click', function () {
-
+$("#start").on("click", function() {
   function rand() {
-    setTimeout(function () {
-      rand = Math.floor(Math.random() * Math.floor(70967))
+    setTimeout(function() {
+      rand = Math.floor(Math.random() * Math.floor(70967));
       return rand;
     }, 10);
   }
   for (i = 0; i < 20; i++) {
     rand();
 
-    setTimeout(function () {
+    setTimeout(function() {
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function () {
+      xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var myObj = JSON.parse(this.responseText);
           //  console.log(myObj.applist.apps[rand]);
@@ -46,42 +46,49 @@ $('#start').on('click', function () {
           appitemid = selectedApplist.appid;
           appitemname = selectedApplist.name;
           // console.log(appitemid);
-          $('<ul>')
+          $("<ul>")
             .text(appitemid)
-            .appendTo('#Pos');
-          $('<ol>').appendTo(
-            $('<ul>').text(appitemname.substring(0, 15))
-            .appendTo('#GameName'));
+            .appendTo("#Pos");
+          $("<ol>").appendTo(
+            $("<ul>")
+              .text(appitemname.substring(0, 15))
+              .appendTo("#GameName")
+          );
 
           //  console.log(myObj.applist.apps[4534]);
         }
       };
       xmlhttp.open("GET", "/Applist.json", false); // async off
       xmlhttp.send();
-
-    }, 10)
-    setTimeout(function () {
+    }, 10);
+    setTimeout(function() {
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function () {
+      xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var myObj = JSON.parse(this.responseText);
           //  console.log(myObj[appitemid].data.publishers, "this is the url");
           var publishers = myObj[appitemid].data.publishers;
 
-          $('<ol>').appendTo(
-            $('<li>')
-            .text(publishers)
-            .appendTo('#Publisher'));
+          $("<ol>").appendTo(
+            $("<li>")
+              .text(publishers)
+              .appendTo("#Publisher")
+          );
           //console.log(myObj.applist.apps[4534]);
         }
       };
-      xmlhttp.open("GET", "http://store.steampowered.com/api/appdetails?appids=" + appitemid, false);
-      var dirtest = 'http://store.steampowered.com/api/appdetails?appids=' + appitemid;
+      xmlhttp.open(
+        "GET",
+        "http://store.steampowered.com/api/appdetails?appids=" + appitemid,
+        false
+      );
+      var dirtest =
+        "http://store.steampowered.com/api/appdetails?appids=" + appitemid;
       xmlhttp.send();
-    }, 10)
-    setTimeout(function () {
+    }, 10);
+    setTimeout(function() {
       var xmlhttp = new XMLHttpRequest();
-      xmlhttp.onreadystatechange = function () {
+      xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
           var myObj = JSON.parse(this.responseText);
           // console.log(myObj[appitemid].data.publishers, "this is the url");
@@ -89,20 +96,28 @@ $('#start').on('click', function () {
           console.log(myObj[appitemid].data.website);
           var website = myObj[appitemid].data.website;
 
-          $('<ol>').appendTo(
-            $('<li>').text(website)
-            .appendTo($('<a>').attr('href', website).appendTo('#link')));
+          $("<ol>").appendTo(
+            $("<li>")
+              .text(website)
+              .appendTo(
+                $("<a>")
+                  .attr("href", website)
+                  .appendTo("#link")
+              )
+          );
           //console.log(myObj.applist.apps[4534]);
         } else {
-
           //console.log(myObj.applist.apps[4534]);
         }
       };
-      xmlhttp.open("GET", "http://store.steampowered.com/api/appdetails?appids=" + appitemid, false);
-      var dirtest = 'http://store.steampowered.com/api/appdetails?appids=' + appitemid;
+      xmlhttp.open(
+        "GET",
+        "http://store.steampowered.com/api/appdetails?appids=" + appitemid,
+        false
+      );
+      var dirtest =
+        "http://store.steampowered.com/api/appdetails?appids=" + appitemid;
       xmlhttp.send();
-    }, 10)
-
+    }, 10);
   }
-
-})
+});
